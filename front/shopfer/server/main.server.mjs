0c4d01de1261dd7068484afa1bd14346a195087b
FarahAbbes <farahabbes210@gmail.com>
2025-07-22 15:31:52 +0200

    add docker image initial

--- front/shopfer/server/main.server.mjs
+++ front/shopfer/server/main.server.mjs
@@ -1,6 +1,2 @@
 import './polyfills.server.mjs';
-<<<<<<< HEAD
-import{a}from"./chunk-QG2B4M7Q.mjs";import"./chunk-LIQ2IVCT.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
-=======
-import{a}from"./chunk-GWUI6BQI.mjs";import"./chunk-LIQ2IVCT.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
->>>>>>> decb7f43721871ffee64dd6e08e1ae9e69f34be7
+import{a}from"./chunk-OBHCS7G3.mjs";import"./chunk-LIQ2IVCT.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
